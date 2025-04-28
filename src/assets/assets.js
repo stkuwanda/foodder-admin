970a2f03aaf1becf277de454fe2cb3ab684c9f77
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-04-28 11:09:47 +0200

    adding signin button and basic styling, adding basic hover options under profile image

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,7 @@ import order_icon from './order_icon.png';
 import profile_image from './profile_image.png';
 import upload_area from './upload_area.png';
 import parcel_icon from './parcel_icon.png';
+import logout_icon from './logout_icon.png';
 
 export const assets = {
 	logo,
@@ -12,6 +13,7 @@ export const assets = {
 	profile_image,
 	upload_area,
 	parcel_icon,
+	logout_icon
 };
 
 export const url = 'http://localhost:4000';
--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -1,15 +1,31 @@
 import { assets } from '../../assets/assets';
 import './NavBar.component.css';
 
-import React from 'react'
+import React from 'react';
 
 function NavBar() {
-  return (
-    <nav>
-      <img className='logo' src={assets.logo} alt="Image of logo" />
-      <img className='profile' src={assets.profile_image} alt="Profile image" />
-    </nav>
-  )
+	return (
+		<nav>
+			<img className='logo' src={assets.logo} alt='Image of logo' />
+
+			<div className='navbar-right'>
+				<button>sign in</button>
+				<div className='navbar-profile'>
+					<img
+						className='profile'
+						src={assets.profile_image}
+						alt='Profile image'
+					/>
+					<ul className='navbar-profile-dropdown'>
+						<li>
+							<img src={assets.logout_icon} alt='Image of a logout icon' />
+							<p>Logout</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default NavBar;
