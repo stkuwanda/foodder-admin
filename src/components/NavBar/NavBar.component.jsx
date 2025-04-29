f65061fcdfd6b209ca74cbd7b6a354a15f880403
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-04-29 07:25:35 +0200

    adding more options to profile menu

--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { assets } from '../../assets/assets';
 import React from 'react';
 import { useAuthContext } from '../../contexts/AuthContext/AuthContext.tools';
 import './NavBar.component.css';
 
 function NavBar() {
+	const navigate = useNavigate();
 	const { isLoggedIn, logout, setIsLoginPopup } = useAuthContext();
 
 	return (
@@ -22,6 +23,21 @@ function NavBar() {
 							alt='Profile image'
 						/>
 						<ul className='navbar-profile-dropdown'>
+							<li onClick={() => navigate('/add')}>
+								<img src={assets.add_icon} alt='Image of a order icon' />
+								<p>Add</p>
+							</li>
+							<hr />
+							<li onClick={() => navigate('/list')}>
+								<img src={assets.order_icon} alt='Image of a order icon' />
+								<p>List</p>
+							</li>
+							<hr />
+							<li onClick={() => navigate('/orders')}>
+								<img src={assets.order_icon} alt='Image of a order icon' />
+								<p>Orders</p>
+							</li>
+							<hr />
 							<li onClick={logout}>
 								<img src={assets.logout_icon} alt='Image of a logout icon' />
 								<p>Logout</p>
